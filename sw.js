889acf24aa60ee807c3868977d827bfbd14ca45e
josephinaiyambo <josephinaiyambo05@gmail.com>
2026-07-09 16:35:34 +0200

    Feat: Added PWA support with manifest and sw.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'safecalc-v1';
+const CACHE_NAME = 'safecalc-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -11,10 +11,23 @@ const ASSETS = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
 
 self.addEventListener('fetch', event => {
   event.respondWith(
